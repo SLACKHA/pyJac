41e5cf4b8b941f4973b4ad847d748335fc3300e2
Nick-lab <nicholas.curtis@uconn.edu>
2014-08-14 10:15:09 -0400

    exp10 and pow10 are very poorly implemented on our version of gcc, returning 1 no matter the input arguement.
    Instead of using these non-standard methods, I changed the c version to simply use pow(10.0, ...)
    
    Signed-off-by: Nick-lab <nicholas.curtis@uconn.edu>

--- rate_subs.py
+++ rate_subs.py
@@ -858,16 +858,23 @@ def write_rxn_pressure_mod(path, lang, specs, reacs):
                         utils.line_end[lang]
                         )
                 file.write(line)
+
+                #exp_10  = dict(c = "pow(10.0, ", cuda = 'exp10(',
+                #fortran = 'exp(log(10)', matlab = 'exp(log(10.0)')
                 
                 line = '  pres_mod'
-                if lang in ['c', 'cuda']:
+                if lang == 'c':
+                    line += ('[{}]'.format(pind) + 
+                             ' = pow(10.0, '
+                             )
+                elif lang == 'cuda':
                     line += ('[{}]'.format(pind) + 
                              ' = exp10('
                              )
                 elif lang in ['fortran', 'matlab']:
                     # fortran & matlab don't have exp10
                     line += ('({})'.format(pind + 1) + 
-                             ' = exp(log(10.0) * '
+                             ' = exp(log(10.0), '
                              )
                 line += 'logFcent / (1.0 + A * A / (B * B))) '
                 

e43d7db3ad22ea83a3614ca5cd970253237d873b
Nick-lab <nicholas.curtis@uconn.edu>
2015-08-03 15:16:23 -0400

    wrote the wrong array to file

--- test.py
+++ test.py
@@ -129,108 +129,108 @@ def convert_mech(mech_filename, therm_filename=None):
 
 def __write_fd_jacob(file, lang, atol=1e-16, rtol=1e-8):
     file.write(
-    """
-    #define FD_ORD 6
+"""
+#define FD_ORD 6
 
-    void eval_fd_jacob (const double t, const double pres, double * y, double * jac) {
+void eval_fd_jacob (const double t, const double pres, double * y, double * jac) {
 
-      double ydot[NN] = {0};
-      dydt (t, pres, y, ydot);
+  double ydot[NN] = {0};
+  dydt (t, pres, y, ydot);
 
-      // Finite difference coefficients
-      double x_coeffs[FD_ORD];
-      double y_coeffs[FD_ORD];
+  // Finite difference coefficients
+  double x_coeffs[FD_ORD];
+  double y_coeffs[FD_ORD];
 
-      if (FD_ORD == 2) {
-        // 2nd order central difference
-        x_coeffs[0] = -1.0;
-        x_coeffs[1] = 1.0;
-        y_coeffs[0] = -0.5;
-        y_coeffs[1] = 0.5;
-      } else if (FD_ORD == 4) {
-        // 4th order central difference
-        x_coeffs[0] = -2.0;
-        x_coeffs[1] = -1.0;
-        x_coeffs[2] = 1.0;
-        x_coeffs[3] = 2.0;
-        y_coeffs[0] = 1.0 / 12.0;
-        y_coeffs[1] = -2.0 / 3.0;
-        y_coeffs[2] = 2.0 / 3.0;
-        y_coeffs[3] = -1.0 / 12.0;
-      } else {
-        // 6th order central difference
-        x_coeffs[0] = -3.0;
-        x_coeffs[1] = -2.0;
-        x_coeffs[2] = -1.0;
-        x_coeffs[3] = 1.0;
-        x_coeffs[4] = 2.0;
-        x_coeffs[5] = 3.0;
+  if (FD_ORD == 2) {
+    // 2nd order central difference
+    x_coeffs[0] = -1.0;
+    x_coeffs[1] = 1.0;
+    y_coeffs[0] = -0.5;
+    y_coeffs[1] = 0.5;
+  } else if (FD_ORD == 4) {
+    // 4th order central difference
+    x_coeffs[0] = -2.0;
+    x_coeffs[1] = -1.0;
+    x_coeffs[2] = 1.0;
+    x_coeffs[3] = 2.0;
+    y_coeffs[0] = 1.0 / 12.0;
+    y_coeffs[1] = -2.0 / 3.0;
+    y_coeffs[2] = 2.0 / 3.0;
+    y_coeffs[3] = -1.0 / 12.0;
+  } else {
+    // 6th order central difference
+    x_coeffs[0] = -3.0;
+    x_coeffs[1] = -2.0;
+    x_coeffs[2] = -1.0;
+    x_coeffs[3] = 1.0;
+    x_coeffs[4] = 2.0;
+    x_coeffs[5] = 3.0;
 
-        y_coeffs[0] = -1.0 / 60.0;
-        y_coeffs[1] = 3.0 / 20.0;
-        y_coeffs[2] = -3.0 / 4.0;
-        y_coeffs[3] = 3.0 / 4.0;
-        y_coeffs[4] = -3.0 / 20.0;
-        y_coeffs[5] = 1.0 / 60.0;
-      }
+    y_coeffs[0] = -1.0 / 60.0;
+    y_coeffs[1] = 3.0 / 20.0;
+    y_coeffs[2] = -3.0 / 4.0;
+    y_coeffs[3] = 3.0 / 4.0;
+    y_coeffs[4] = -3.0 / 20.0;
+    y_coeffs[5] = 1.0 / 60.0;
+  }
 
-      double ewt[NN];
-      #pragma unroll
-      """
-    )
+  double ewt[NN];
+  #pragma unroll
+  """
+)
     file.write(
-    """
-      for (int i = 0; i < NN; ++i) {{
-        ewt[i] = {} + ({} * fabs(y[i]));
-      }}
+"""
+  for (int i = 0; i < NN; ++i) {{
+    ewt[i] = {} + ({} * fabs(y[i]));
+  }}
 
-      // unit roundoff of machine
-      double srur = sqrt(DBL_EPSILON);
+  // unit roundoff of machine
+  double srur = sqrt(DBL_EPSILON);
 
-      double sum = 0.0;
-      #pragma unroll
-      for (int i = 0; i < NN; ++i) {{
-        sum += (ewt[i] * ydot[i]) * (ewt[i] * ydot[i]);
-      }}
-      double fac = sqrt(sum / ((double)(NN)));
-      double r0 = 1000.0 * {} * DBL_EPSILON * ((double)(NN)) * fac;
-      """.format(atol, rtol, rtol)
-    )
+  double sum = 0.0;
+  #pragma unroll
+  for (int i = 0; i < NN; ++i) {{
+    sum += (ewt[i] * ydot[i]) * (ewt[i] * ydot[i]);
+  }}
+  double fac = sqrt(sum / ((double)(NN)));
+  double r0 = 1000.0 * {} * DBL_EPSILON * ((double)(NN)) * fac;
+  """.format(atol, rtol, rtol)
+)
     file.write(
-    """
-      double ftemp[NN] = {0};
+"""
+  double ftemp[NN] = {0};
 
-      #pragma unroll
-      for (int j = 0; j < NN; ++j) {
-        double yj_orig = y[j];
-        double r = fmax(srur * fabs(yj_orig), r0 / ewt[j]);
+  #pragma unroll
+  for (int j = 0; j < NN; ++j) {
+    double yj_orig = y[j];
+    double r = fmax(srur * fabs(yj_orig), r0 / ewt[j]);
 
-        #pragma unroll
-        for (int i = 0; i < NN; ++i) {
-          jac[i + NN*j] = 0.0;
-        }
-
-        #pragma unroll
-        for (int k = 0; k < FD_ORD; ++k) {
-          y[j] = yj_orig + x_coeffs[k] * r;
-          dydt (t, pres, y, ftemp);    
-          #pragma unroll
-          for (int i = 0; i < NN; ++i) {
-            jac[i + NN*j] += y_coeffs[k] * ftemp[i];
-          }
-        }
-
-        #pragma unroll
-        for (int i = 0; i < NN; ++i) {
-          jac[i + NN*j] /= r;
-        }
-
-        y[j] = yj_orig;
-      }
-
+    #pragma unroll
+    for (int i = 0; i < NN; ++i) {
+      jac[i + NN*j] = 0.0;
     }
-    """
-    )
+
+    #pragma unroll
+    for (int k = 0; k < FD_ORD; ++k) {
+      y[j] = yj_orig + x_coeffs[k] * r;
+      dydt (t, pres, y, ftemp);    
+      #pragma unroll
+      for (int i = 0; i < NN; ++i) {
+        jac[i + NN*j] += y_coeffs[k] * ftemp[i];
+      }
+    }
+
+    #pragma unroll
+    for (int i = 0; i < NN; ++i) {
+      jac[i + NN*j] /= r;
+    }
+
+    y[j] = yj_orig;
+  }
+
+}
+"""
+)
 
 
 def __write_header_include(file, lang):
@@ -538,7 +538,7 @@ def write_c_test(build_dir, pmod):
             '  write_jacob (fp, jacob);\n'
             '  double fd_jacob[NN * NN] = {0};\n'
             '  eval_fd_jacob(0.0, pres, y, fd_jacob);\n'
-            '  write_jacob(fp, jacob);\n'
+            '  write_jacob(fp, fd_jacob);\n'
             '\n'
             '  fclose (fp);\n'
             '\n'

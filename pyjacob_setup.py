22562675a88858175468980faced24aed595e563
arghdos <arghdos@users.noreply.github.com>
2015-08-26 09:32:01 -0400

    actually added cython files

---
+++ pyjacob_setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup, Extension
+#! /usr/bin/env python2.7
+
+from Cython.Distutils import build_ext
+import numpy
+ext_modules=[Extension("pyjacob", 
+	sources=["pyjacob_wrapper.pyx", 
+			'out/dydt.c', 
+			'out/rxn_rates.c',
+			'out/rxn_rates_pres_mod.c',
+			'out/spec_rates.c',
+			'out/chem_utils.c'],
+    include_dirs=['out/', numpy.get_include()],
+    language='c',
+    )]
+
+setup(
+	name='pyjacob',
+	ext_modules=ext_modules,
+    cmdclass={'build_ext': build_ext}
+)

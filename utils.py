b88e882fd922d3b13f6c32dde8ef909e16693acc
Kyle Niemeyer <kyle.niemeyer@gmail.com>
2015-10-28 15:42:47 -0400

    formatting fixes in utils.py

--- utils.py
+++ utils.py
@@ -40,6 +40,7 @@ array_chars = dict(c="[{}]", cuda="[{}]",
                    fortran="({})", matlab="({})"
                    )
 
+
 def get_nu(isp, rxn):
     """Returns the net nu of species isp for the reaction rxn
     """
@@ -57,6 +58,7 @@ def get_nu(isp, rxn):
         # doesn't participate in reaction
         return 0
     return nu
+
 
 def read_str_num(string, sep=None):
     """Returns a list of floats pulled from a string.
@@ -125,8 +127,8 @@ def create_dir(path):
 
 def get_array(lang, name, index, twod=None):
     """
-    Given a language and an index, returns the proper string index formatted into the appropriate array characters (
-    e.g. [] or ())
+    Given a language and an index, returns the proper string index formatted
+    into the appropriate array characters (e.g., [] or ()).
 
     Parameters
     ----------
@@ -137,7 +139,8 @@ def get_array(lang, name, index, twod=None):
     index : int
         The index to format
     twod : int, optional
-        If not None and the lang is fortan or matlab this will be formatted as a second index in the array
+        If not None and the lang is fortan or matlab this will be formatted
+        as a second index in the array.
     """
     if index is None:
         #a dummy call to see if it's in shared memory
@@ -152,8 +155,8 @@ def get_array(lang, name, index, twod=None):
 
 def get_index(lang, index):
     """
-    Given an integer index this function will return the proper string version of the index
-    based on the language and other considerations
+    Given an integer index this function will return the proper string
+    version of the index based on the language and other considerations
 
     Parameters
     ----------
@@ -173,10 +176,12 @@ def get_index(lang, index):
     if lang in ['c', 'cuda']:
         return str(index)
 
+
 def reassign_species_lists(reacs, specs):
     """
-    Given a list of ReacInfo, and SpecInfo's, this method will update the ReacInfo's
-    reactants / products / third body list to integers representing the species' index in the list
+    Given a list of ReacInfo, and SpecInfo's, this method will update the
+    ReacInfo's reactants / products / third body list to integers
+    representing the species' index in the list.
     """
 
     species_map = {sp.name: i for i, sp in enumerate(specs)}
@@ -187,8 +192,11 @@ def reassign_species_lists(reacs, specs):
         if rxn.pdep_sp:
             rxn.pdep_sp = species_map[rxn.pdep_sp]
 
+
 def is_integer(val):
-    """Returns whether a value is an integer regardless of whether it's a float or it's an int"""
+    """Returns whether a value is an integer regardless of whether
+    it is a float or an int
+    """
     try:
         return val.is_integer()
     except:
@@ -200,89 +208,108 @@ def is_integer(val):
         except:
             return False
 
+
 def get_parser():
     # command line arguments
     parser = ArgumentParser(description='pyJac: Generates source code '
-                                        'for analytical Jacobian.')
+                                        'for analytical Jacobian.'
+                            )
     parser.add_argument('-l', '--lang',
                         type=str,
                         choices=langs,
                         required=True,
-                        help='Programming language for output '
-                             'source files.')
+                        help='Programming language for output source files.'
+                        )
     parser.add_argument('-i', '--input',
                         type=str,
                         required=True,
-                        help='Input mechanism filename (e.g., mech.dat).')
+                        help='Input mechanism filename (e.g., mech.dat).'
+                        )
     parser.add_argument('-t', '--thermo',
                         type=str,
                         default=None,
                         help='Thermodynamic database filename (e.g., '
-                             'therm.dat), or nothing if in mechanism.')
+                             'therm.dat), or nothing if in mechanism.'
+                        )
     parser.add_argument('-ic', '--initial-conditions',
                         type=str,
                         dest='initial_conditions',
                         default='',
                         required=False,
-                        help='A comma separated list of initial initial conditions to set in the '
+                        help='A comma separated list of initial initial '
+                             'conditions to set in the '
                              'set_same_initial_conditions method. \
                                 Expected Form: T,P,Species1=...,Species2=...,...\n\
                                 Temperature in K\n\
                                 Pressure in Atm\n\
-                                Species in moles')
+                                Species in moles'
+                        )
     # cuda specific
     parser.add_argument('-nco', '--no-cache-optimizer',
                         dest='cache_optimizer',
                         action='store_false',
                         default=True,
-                        help='Do not attempt to optimize cache store/loading via use '
-                             'of a greedy selection algorithm.')
+                        help='Do not attempt to optimize cache store/loading '
+                             'via use of a greedy selection algorithm.'
+                        )
     parser.add_argument('-nosmem', '--no-shared-memory',
                         dest='no_shared',
                         action='store_true',
                         default=False,
-                        help='Use this option to turn off attempted shared memory acceleration for CUDA')
+                        help='Use this option to turn off attempted shared '
+                             'memory acceleration for CUDA.'
+                        )
     parser.add_argument('-pshare', '--prefer-shared',
                         dest='L1_preferred',
                         action='store_false',
                         default=True,
                         help='Use this option to allocate more space for '
-                             'shared memory than the L1 cache for CUDA (not recommended)')
+                             'shared memory than the L1 cache for CUDA '
+                             '(not recommended).'
+                        )
     parser.add_argument('-nb', '--num-blocks',
                         type=int,
                         dest='num_blocks',
                         default=8,
                         required=False,
-                        help='The target number of blocks / sm to achieve for CUDA.')
+                        help='The target number of blocks / sm for CUDA.'
+                        )
     parser.add_argument('-nt', '--num-threads',
                         type=int,
                         dest='num_threads',
                         default=64,
                         required=False,
-                        help='The target number of threads / block to achieve for CUDA.')
+                        help='The target number of threads / block for CUDA.'
+                        )
     parser.add_argument('-mt', '--multi-threaded',
                         type=int,
                         dest='multi_thread',
                         default=None,
                         required=False,
-                        help='The number of threads to use during the optimization process')
+                        help='The number of threads to use during the '
+                             'optimization process.'
+                        )
     parser.add_argument('-fopt', '--force-optimize',
                         dest='force_optimize',
                         action='store_true',
                         default=False,
-                        help='Use this option to force a reoptimization of the mechanism '
-                             '(usually only happens when generating for a different mechanism)')
+                        help='Use this option to force a reoptimization of '
+                             'the mechanism (usually only happens when '
+                             'generating for a different mechanism).'
+                        )
     parser.add_argument('-b', '--build_path',
                         required=False,
                         default='./out/',
-                        help='The folder to generate the mechanism in')
+                        help='The folder to generate the mechanism in.'
+                        )
     parser.add_argument('-ls', '--last_species',
                         required=False,
                         type=str,
                         default=None,
-                        help='The name of the species to set as the last in the mechanism.  '
-                             'If not specifed, defaults to thge first of N2, AR, and HE '
-                             'in the mechanism')
+                        help='The name of the species to set as the last in '
+                             'the mechanism. If not specifed, defaults to '
+                             'the first of N2, AR, and HE in the mechanism.'
+                        )
 
     args = parser.parse_args()
     return args

21f5ec1a5780a9f460fd88f136d39218af9d5602
Kyle Niemeyer <kyle.niemeyer@gmail.com>
2015-12-10 12:26:23 -0800

    added parallel compilation to pyjacob_setup

--- pyjacob_setup.py
+++ pyjacob_setup.py
@@ -1,9 +1,35 @@
 #! /usr/bin/env python2.7
 from distutils.core import setup, Extension
+import distutils.ccompiler
 
 from Cython.Distutils import build_ext
+from multiprocessing.pool import ThreadPool
 import numpy
 import os
+
+# monkey-patch for parallel compilation
+def parallelCCompile(self, sources, output_dir=None, macros=None,
+                     include_dirs=None, debug=0, extra_preargs=None,
+                     extra_postargs=None, depends=None):
+    # those lines are copied from distutils.ccompiler.CCompiler directly
+    macros, objects, extra_postargs, pp_opts, build = self._setup_compile(
+        output_dir, macros, include_dirs, sources, depends, extra_postargs
+        )
+    cc_args = self._get_cc_args(pp_opts, debug, extra_preargs)
+
+    # number of parallel compilations
+    N = 8
+
+    def _single_compile(obj):
+        try: src, ext = build[obj]
+        except KeyError: return
+        self._compile(obj, src, ext, cc_args, extra_postargs, pp_opts)
+
+    # convert to list, imap is evaluated on-demand
+    list(ThreadPool(N).imap(_single_compile,objects))
+    return objects
+
+distutils.ccompiler.CCompiler.compile = parallelCCompile
 
 sources = ['pyjacob_wrapper.pyx',
            'out/dydt.c',

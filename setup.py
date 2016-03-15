b9567bfb6c1599f5e4177e5deef6a20be1241cdc
arghdos <arghdos@gmail.com>
2016-03-15 13:56:55 -0400

    requires numpy

--- setup.py
+++ setup.py
@@ -74,6 +74,7 @@ setup(
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     # install_requires=['peppercorn'],
+    install_requires=['numpy']
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,

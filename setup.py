32e0960dff1ce3905833f00554fb0027628aa6eb
arghdos <arghdos@gmail.com>
2016-03-17 10:55:45 -0400

    added bit array

--- setup.py
+++ setup.py
@@ -74,7 +74,8 @@ setup(
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     # install_requires=['peppercorn'],
-    install_requires=['numpy']
+    install_requires=['numpy',
+                      'bitarray']
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,

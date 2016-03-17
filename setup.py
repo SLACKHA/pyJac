6b4bc4342cd21aae9aa0d9555945056f8efde135
arghdos <arghdos@gmail.com>
2016-03-17 11:02:18 -0400

    bump up the version number and update setup.py

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
@@ -63,7 +63,7 @@ setup(
     ],
 
     # What does your project relate to?
-    keywords='chemical_kinetics',
+    keywords='chemical_kinetics analytical jacobian',
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().

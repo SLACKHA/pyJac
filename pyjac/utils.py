555c4fabfcd1e77e52096944bd376b0860e375b2
arghdos <arghdos@gmail.com>
2016-05-06 12:56:32 -0400

    turn off cache optimizer by default

--- pyjac/utils.py
+++ pyjac/utils.py
@@ -354,12 +354,12 @@ def get_parser():
                                 Species in moles'
                         )
     # cuda specific
-    parser.add_argument('-nco', '--no-cache-optimizer',
+    parser.add_argument('-co', '--cache-optimizer',
                         dest='cache_optimizer',
-                        action='store_false',
-                        default=True,
-                        help='Do not attempt to optimize cache store/loading '
-                             'via use of a greedy selection algorithm.'
+                        action='store_true',
+                        default=False,
+                        help='Attempt to optimize cache store/loading '
+                             'via use of a greedy selection algorithm. (Experimental)'
                         )
     parser.add_argument('-nosmem', '--no-shared-memory',
                         dest='no_shared',

2d74265d4596e49d1d6a2bc6be5207dc17bec971
arghdos <arghdos@gmail.com>
2016-11-09 11:06:49 -0500

    make enums convertible to/from int, cannot use IntEnum as this allows comparison between different enums

--- pyjac/core/reaction_types.py
+++ pyjac/core/reaction_types.py
@@ -7,20 +7,28 @@ class reaction_type(Enum):
     chem = 4
     plog = 5
     cheb = 6
+    def __int__(self):
+        return self.value
 
 class thd_body_type(Enum):
     none = 0
     mix = 1
     species = 2
     unity = 3
+    def __int__(self):
+        return self.value
 
 class falloff_form(Enum):
     none = 0
     lind = 1
     troe = 2
     sri = 3
+    def __int__(self):
+        return self.value
 
 class reversible_type(Enum):
     non_reversible = 1
     explicit = 2
     non_explicit = 3
+    def __int__(self):
+        return self.value

208908b43815ed5b77b38bf3df74dd0d36d5ddae
arghdos <arghdos@gmail.com>
2016-11-16 11:00:41 -0500

    add subs test, fix symlist test

--- pyjac/tests/test_sympy_utils.py
+++ pyjac/tests/test_sympy_utils.py
@@ -6,7 +6,7 @@ from ..sympy import sympy_utils as sp_util
 import sympy as sp
 
 class SubTest(TestClass):
-    def test_subs(self):
+    def test_symlist(self):
         #make symbols
         A = sp.IndexedBase('A')
         B = sp.IndexedBase('B')
@@ -17,6 +17,30 @@ class SubTest(TestClass):
         A_dummy = sp.Symbol('A[i]')
         B_dummy = sp.Symbol('B[i]')
 
+        free_syms = {A_dummy, B_dummy}
         expr_san = sp_util.sanitize(expr)
-        assert len(expr.free_symbols.intersection({A_dummy, B_dummy, i})) ==\
-                    len(expr.free_symbols)
+        assert expr_san.free_symbols.intersection(free_syms) == free_syms
+
+        expr_san = sp_util.sanitize(expr, symlist={
+            'A[i]' : A_dummy,
+            'B[i]' : B_dummy
+            })
+        assert expr_san.free_symbols.intersection(free_syms) == free_syms
+
+    def test_subs(self):
+        #make symbols
+        A = sp.IndexedBase('A')
+        B = sp.IndexedBase('B')
+        i = sp.Idx('i')
+
+        expr = A[i] + B[i]
+
+        A_dummy = sp.Symbol('A[i]')
+        B_dummy = sp.Symbol('B[i]')
+        C_dummy = sp.Symbol('C[i]')
+
+
+        expr_san = sp_util.sanitize(expr, subs={
+            A_dummy + B_dummy : C_dummy
+            })
+        assert expr_san == C_dummy

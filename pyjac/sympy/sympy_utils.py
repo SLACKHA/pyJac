7a7753e4183ec5a071a584b6495c19dbfff3d4bd
arghdos <arghdos@gmail.com>
2017-02-01 19:28:28 -0500

    finally, a fix for the stupid 'sometimes it won't subs' bug!

--- pyjac/sympy/sympy_utils.py
+++ pyjac/sympy/sympy_utils.py
@@ -23,8 +23,9 @@ def sanitize(eqn, subs={}, symlist={}):
     """
     #first we replace values with regular symbols for easy working
     #if an equivalent value is not found in the symlist the str conversion of the symbol will be used
+    tosubs = [x for x in eqn.free_symbols if not any(y.has(x) and x != y for y in eqn.free_symbols)]
     indexed = [(x, next((sym for name, sym in symlist.items()
-                if name == str(x)), Symbol(str(x)))) for x in eqn.free_symbols]
+                if name == str(x)), Symbol(str(x)))) for x in tosubs]
     eqn = eqn.subs(indexed)
     #next we do any complex user specified substitution
     eqn = eqn.subs([(k, v) for k, v in subs.items()])

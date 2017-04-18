a6aa8a8fe0fa450f780404dfca571f34ea070b30
Kyle Niemeyer <kyle.niemeyer@gmail.com>
2017-04-17 22:48:46 -0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -4,118 +4,92 @@ https://packaging.python.org/en/latest/distributing.html
 https://github.com/pypa/sampleproject
 """
 
-# Always prefer setuptools over distutils
-from setuptools import setup, find_packages
-# To use a consistent encoding
+from setuptools import setup
 from codecs import open
 from os import path
-
-with open('pyjac/_version.py') as version_file:
-    exec(version_file.read())
+import sys
 
 here = path.abspath(path.dirname(__file__))
 
-# Get the long description from the relevant file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+with open(path.join(here, 'pyjac', '_version.py')) as version_file:
+    exec(version_file.read())
+
+# Get the long description from the relevant files
+with open(path.join(here, 'README.rst'), encoding='utf-8') as readme_file:
+    readme = readme_file.read()
+
+with open(path.join(here, 'CHANGELOG.md')) as changelog_file:
+    changelog = changelog_file.read()
+
+with open(path.join(here, 'CITATION.md')) as citation_file:
+    citation = citation_file.read()
+
+desc = changelog + '\n\n' + citation
+try:
+    import pypandoc
+    long_description = pypandoc.convert_text(desc, 'rst', format='md')
+    long_description = readme + '\n\n' + long_description
+    with open(path.join(here, 'README.rst'), 'w') as rst_readme:
+        rst_readme.write(long_description)
+except (ImportError, OSError, IOError):
+    long_description = readme + '\n\n' + desc
+
+install_requires = [
+    'numpy>=1.9.0',
+    'bitarray>=0.8.1',
+    'optionloop>=1.0.4',
+]
+
+tests_require = [
+    'Cython>=0.23.1',
+    'pyyaml>=3.11',
+]
 
 setup(
-    name='pyJac',
-
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
+    name='pyjac',
     version=__version__,
-
     description='Create analytical Jacobian matrix source code for chemical kinetics',
     long_description=long_description,
-
-    # The project's main homepage.
     url='https://github.com/slackha/pyJac',
-
-    # Author details
     author='Kyle E. Niemeyer',
     author_email='kyle.niemeyer@gmail.com',
-
-    # Choose your license
     license='MIT License',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 5 - Production/Stable',
-
-        # Indicate who your project is intended for
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
-
-        # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
-
         'Natural Language :: English',
         'Operating System :: OS Independent',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
-
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Physics',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-
-    # What does your project relate to?
     keywords='chemical_kinetics analytical_Jacobian',
 
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['docs']),
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    # install_requires=['peppercorn'],
-    install_requires=['numpy',
-                      'bitarray',
-                      'optionloop'],
-
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-    # extras_require={
-    #     'dev': ['check-manifest'],
-    #     'test': ['coverage'],
-    # },
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
+    packages=['pyjac', 'pyjac.core', 'pyjac.functional_tester', 'pyjac.libgen',
+              'pyjac.performance_tester', 'pyjac.pywrap', 'pyjac.utils',
+              ],
+    package_dir={'pyjac': 'pyjac'},
+    install_requires=install_requires,
     package_data={
         'pyjac.pywrap': ['*.pyx', '*.c', '*.h', '*.cu', '*.cuh', '*.in'],
         'pyjac.functional_tester' : ['*.yaml'],
         'pyjac.performance_tester' : ['*.pyx', '*.c', '*.h', '*.cu',
                                       '*.cuh', '*.in'
                                       ],
-    },
+        },
     include_package_data=True,
+    tests_require=tests_require,
 
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    # data_files=[('my_data', ['data/data_file'])],
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
             'pyjac=pyjac.__main__:main',
